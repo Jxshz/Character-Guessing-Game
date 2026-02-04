8f9892feb85602b6ce99b2abdec2f77399128942
JOSHVA A <Joshanbirin@gmail.com>
2026-02-04 19:34:37 +0530

    Refactor word guessing game with user name and feedback
    Refactor word guessing game to include user name input and improve structure. Adjust word lists to lowercase and enhance user feedback.

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,87 +1,52 @@
 import random
 
-answer = input("Marvel or DC? ").strip()
+name = input("What's your name: ")
+print(f"Looking forward to see you {name}!!")
+
+answer = input("What's your favourite ? Marvel or DC? ").strip()
 
 if answer == "Marvel":
     print("I got a Marvel fan! Get ready to jump in MCU")
-    words = ['Ironman', 'Spiderman', 'Hulk', 'Black widow', 'Captain america',
-             'Hawkeye', 'Thanos', 'Doctor strange']
-    while turns > 0:
-    failed = 0
-
-    for char in word:
-        if char in guesses:
-            print(char, end=" ")
-        else:
-            print("_", end=" ")
-            failed += 1
-
-    print()
-
-    if failed == 0:
-        print("\nYou Win")
-        print("The word is:", word)
-        break
-
-    guess = input("Guess a character: ")
-
-    guesses += guess
-
-    if guess not in word:
-        turns -= 1
-        print("Wrong")
-        print("You have", turns, 'more guesses')
-
-        if turns == 0:
-            print("You Loose")
-            print("The word was:", word)
-
+    words = ['ironman', 'spiderman', 'hulk', 'black widow', 'captain america', 'hawkeye', 'thanos', 'doctor strange']
 elif answer == "DC":
     print("I got a DC fan! Get ready to jump in DCU")
-    words = ['Batman', 'Superman', 'Aquaman', 'Wonderwoman', 'Flash']
-    while turns > 0:
-    failed = 0
-
-    for char in word:
-        if char in guesses:
-            print(char, end=" ")
-        else:
-            print("_", end=" ")
-            failed += 1
-
-    print()
-
-    if failed == 0:
-        print("\nYou Win")
-        print("The word is:", word)
-        break
-
-    guess = input("Guess a character: ")
-
-    guesses += guess
-
-    if guess not in word:
-        turns -= 1
-        print("Wrong")
-        print("You have", turns, 'more guesses')
-
-        if turns == 0:
-            print("You Loose")
-            print("The word was:", word)
-
+    words = ['batman', 'superman', 'aquaman', 'wonderwoman', 'flash']
 else:
-    print("Watch some movies kiddo !")
-    words = ['ironman', 'spiderman', 'hulk', 'black widow', 'captain america',
-             'hawkeye', 'thanos', 'doctor strange', 'batman', 'reverse', 'water', 'board', 'geeks']
+    print("Watch some movies kiddo!")
+    words = []
 
-name = input("Enter your name: ")
-print(f"Looking forward to see you {name}!")
+if words:
+    word = random.choice(words).lower()
+    guesses = ''
+    turns = 5
 
-word = random.choice(words)
+    print("\nGuess the characters!")
 
-print("Guess the characters")
+    while turns > 0:
+        failed = 0
 
-guesses = ''
-turns = 5
+        for char in word:
+            if char == " ":
+                print(" ", end=" ")
+            elif char in guesses:
+                print(char, end=" ")
+            else:
+                print("_", end=" ")
+                failed += 1
 
+        if failed == 0:
+            print("\n\n🏆 You Win!")
+            print(f"The word was: {word}")
+            break
 
+        print()
+        guess = input("\nGuess a letter: ").lower()
+        guesses += guess
+
+        if guess not in word:
+            turns -= 1
+            print(f"❌ Wrong! You have {turns} more guesses.")
+
+            if turns == 0:
+                print("\n💀 You Lose")
+                print(f"The word was: {word}")
